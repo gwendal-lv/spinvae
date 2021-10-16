4af6bb877bc85e944e7c3526ffb7025cc57afb65
Gwendal Le Vaillant <gwendallv.dev@gmail.com>
2021-10-16 20:32:17 +0200

    ResBlock3Layers - no up/down sampling yet

--- model/convlayer.py
+++ model/convlayer.py
@@ -2,9 +2,11 @@
 """
 Defines some basic layer Classes to be integrated into bigger networks
 """
+import copy
 from typing import Optional
 from abc import ABC, abstractmethod
 
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -32,59 +34,69 @@ class AdaIN(nn.Module):
 
 
 class ActAndNorm(nn.Module):
-    def __init__(self, out_ch, act=nn.ReLU(),
-                 norm_layer: Optional[str] = 'bn', adain_num_style_features: Optional[int] = None):
+    def __init__(self, in_ch, out_ch, act=nn.ReLU(),
+                 norm_layer: Optional[str] = 'bn', adain_num_style_features: Optional[int] = None, reverse_order=False):
         """
         Base class for any conv layer with act and norm (usable a base class for Conv or Transpose Conv layers,
         or as main class). The forward function returns a batch of features maps, and a batch of w conditioning vectors.
 
         :param norm_layer: 'bn' (Batch Norm), 'adain' (Adaptive Instance Norm, requires w_style during forward) or None
+        :param reverse_order: Ff False, conv->act->norm. If True, norm->act->conv (res blocks)
         """
         super().__init__()
+        self.reverse_order = reverse_order
         self.conv = None  # can be assigned by child class - otherwise, not applied
         self.act = act
         self.norm_layer_description = norm_layer
         if self.norm_layer_description is None:
             self.norm = None
         elif self.norm_layer_description == 'bn':
-            self.norm = nn.BatchNorm2d(out_ch)
+            self.norm = nn.BatchNorm2d(out_ch if not reverse_order else in_ch)
         elif self.norm_layer_description == 'adain':
-            self.norm = AdaIN(adain_num_style_features, out_ch)
+            self.norm = AdaIN(adain_num_style_features, out_ch if not reverse_order else in_ch)
         else:
             raise AssertionError("Layer norm {} not available.".format(self.norm_layer_description))
 
     def forward(self, x_and_w_style):
         """ Always returns a tuple (x, w) for the style to be passed to the next layer in the sequence. """
         x, w_style = x_and_w_style[0], x_and_w_style[1]
-        if self.conv is not None:
-            x = self.conv(x)
-        x = self.act(x)
+        if not self.reverse_order:  # Usual order: conv->act->norm
+            if self.conv is not None:
+                x = self.conv(x)
+            x = self.act(x)
         if self.norm_layer_description == 'bn':
-            return self.norm(x), w_style
+            x = self.norm(x)
         elif self.norm_layer_description == 'adain':
             if w_style is None:
                 raise AssertionError("w_style cannot be None when using AdaIN")
-            return self.norm(x, w_style), w_style
+            x = self.norm(x, w_style)
         else:
-            return x, w_style
+            pass
+        if self.reverse_order:  # reversed order: norm->act->conv (used in res blocks)
+            x = self.act(x)
+            if self.conv is not None:
+                x = self.conv(x)
+        return x, w_style
 
 
 class Conv2D(ActAndNorm):
     """ A basic conv layer with activation and normalization layers """
     def __init__(self, in_ch, out_ch, kernel_size, stride, padding, dilation=(1, 1),
                  padding_mode='zeros', act=nn.ReLU(),
-                 norm_layer: Optional[str] = 'bn', adain_num_style_features: Optional[int] = None):
-        super().__init__(out_ch, act, norm_layer, adain_num_style_features)
+                 norm_layer: Optional[str] = 'bn', adain_num_style_features: Optional[int] = None, reverse_order=False):
+        super().__init__(in_ch, out_ch, act, norm_layer, adain_num_style_features, reverse_order)
         self.conv = nn.Conv2d(in_ch, out_ch, kernel_size, stride, padding, dilation, padding_mode=padding_mode)
 
 
 class TConv2D(ActAndNorm):
     """ A basic Transposed conv layer with activation and normalization layers """
     def __init__(self, in_ch, out_ch, kernel_size, stride, padding, output_padding=0, dilation=1, padding_mode='zeros',
-                 act=nn.ReLU(), norm_layer: Optional[str] = 'bn', adain_num_style_features: Optional[int] = None):
-        super().__init__(out_ch, act, norm_layer, adain_num_style_features)
+                 act=nn.ReLU(),
+                 norm_layer: Optional[str] = 'bn', adain_num_style_features: Optional[int] = None, reverse_order=False):
+        super().__init__(in_ch, out_ch, act, norm_layer, adain_num_style_features, reverse_order)
         self.conv = nn.ConvTranspose2d(in_ch, out_ch, kernel_size, stride, padding, output_padding,
                                        dilation=dilation, padding_mode=padding_mode)
+
 
 
 class DenseConv2D(nn.Module):
@@ -149,6 +161,7 @@ class DenseTConv2D(nn.Module):
         return self.bn2(self.act2(x_cat))
 
 
+
 class ResConv2DBase(nn.Module, ABC):
     """ Base class for ResConv2D and ResTConv2D: 2 convolutional layers with input added to the output
     (if necessary: number of channels adapted using a 1x1 conv).
@@ -160,10 +173,10 @@ class ResConv2DBase(nn.Module, ABC):
         self.conv1_act_bn = None  # to be set by child class
         self.conv2 = None
         self.residuals_conv = nn.Conv2d(in_ch, out_ch, (1, 1)) if (in_ch != out_ch) else None
-        self.act_norm_2 = ActAndNorm(out_ch, act, norm_layer, adain_num_style_features)
+        self.act_norm_2 = ActAndNorm(in_ch, out_ch, act, norm_layer, adain_num_style_features)
 
     @abstractmethod
-    def _resize_residuals(self, res, output_feature_maps_size):
+    def _resize_x_skip(self, x_skip, output_feature_maps_size):
         pass
 
     def forward(self, x_and_w):
@@ -171,11 +184,11 @@ class ResConv2DBase(nn.Module, ABC):
         x_hidden, _ = self.conv1_act_bn((x, w))
         x_before_add = self.conv2(x_hidden)
         output_feature_maps_size = x_before_add.shape[2:4]
-        # Residuals - conv, automatic average pool if necessary
-        res = self.residuals_conv(x) if self.residuals_conv is not None else x
-        if res.shape[2:4] != output_feature_maps_size:
-            res = self._resize_residuals(res, output_feature_maps_size)
-        return self.act_norm_2((x_before_add + res, w))
+        # Skip connection - conv, automatic average pool if necessary
+        x_skip = self.residuals_conv(x) if self.residuals_conv is not None else x
+        if x_skip.shape[2:4] != output_feature_maps_size:
+            x_skip = self._resize_x_skip(x_skip, output_feature_maps_size)
+        return self.act_norm_2((x_before_add + x_skip, w))
 
 
 class ResConv2D(ResConv2DBase):
@@ -189,8 +202,8 @@ class ResConv2D(ResConv2DBase):
                                    padding_mode, act, norm_layer, adain_num_style_features)
         self.conv2 = nn.Conv2d(hidden_ch, out_ch, kernel_size, stride, padding, dilation, padding_mode=padding_mode)
 
-    def _resize_residuals(self, res, output_feature_maps_size):
-        return F.adaptive_avg_pool2d(res, output_feature_maps_size)
+    def _resize_x_skip(self, x_skip, output_feature_maps_size):
+        return F.adaptive_avg_pool2d(x_skip, output_feature_maps_size)
 
 
 class ResTConv2D(ResConv2DBase):
@@ -204,7 +217,88 @@ class ResTConv2D(ResConv2DBase):
         self.conv2 = nn.ConvTranspose2d(hidden_ch, out_ch, kernel_size, stride, padding, output_padding,
                                         dilation=dilation, padding_mode=padding_mode)
 
-    def _resize_residuals(self, res, output_feature_maps_size):
-        return F.interpolate(res, output_feature_maps_size, mode='bilinear', align_corners=False)
+    def _resize_x_skip(self, x_skip, output_feature_maps_size):
+        return F.interpolate(x_skip, output_feature_maps_size, mode='bilinear', align_corners=False)
 
 
+
+class ResBlock3Layers(nn.Module):
+    def __init__(self, in_ch, internal_3x3_ch, out_ch, internal_stride=(1, 1), act=nn.ReLU(),
+                 norm_layer: Optional[str] = 'bn', adain_num_style_features: Optional[int] = None):
+        """
+        The reduced number of channels to perform the actual 3x3 conv is inspired by ResNeXt.
+
+        :param norm_layer: If 'bn' or 'adain', is applied to all norm layers. If 'bn+adain', the middle residual
+            layers uses AdaIN, the 2 others use BN.
+        """
+        super().__init__()
+        self.in_ch, self.out_ch = in_ch, out_ch
+        if norm_layer is None:
+            bn, adain = False, False
+        elif norm_layer.lower() == 'bn':
+            bn, adain = True, False
+        elif norm_layer.lower() == 'adain':
+            bn, adain = False, True
+        elif norm_layer.lower() == 'bn+adain' or norm_layer.lower() == 'adain+bn':
+            bn, adain = True, True
+        else:
+            raise ValueError("'{}' norm layer: unrecognized string argument.".format(norm_layer))
+        if adain and adain_num_style_features is None:
+            raise AssertionError("AdaIN requires adain_num_style_features not to be None.")
+        # Residual network: reduces the nb of channels, applies a 3x3 conv, then increases the nb of ch before add
+        # TODO adapter taille residuals si strided conv (taille feature maps diminue)
+        # TODO adapter nb ch residuals si nb channels diminue
+        #    si nb channels augmente: ajouter résidus sur une partie seulement des ch?
+        self.res_convs = nn.Sequential()
+        # allow a mix of adain and bn --> 1 adain first, all the rest is BN
+        norm_name = 'bn' if bn else ('adain' if adain else None)  # 1st layer: BN has the priority over AdaIN
+        self.res_convs.add_module('redu1x1',
+                                  Conv2D(in_ch, internal_3x3_ch, (1, 1), (1, 1), (0, 0),  # kernel, stride, padding
+                                         act=copy.deepcopy(act), reverse_order=True,
+                                         norm_layer=norm_name, adain_num_style_features=adain_num_style_features))
+
+        norm_name = 'adain' if adain else ('bn' if bn else None)  # 2nd layer: AdaIN has the priority over BN
+        # Strided convolution (downsampling)
+        # TODO bicubic upsampling (3x3 tconv leads to artifacts: https://distill.pub/2016/deconv-checkerboard/)
+        # TODO allow custom input padding (to increase size more)
+        self.res_convs.add_module('conv3x3',
+                                  Conv2D(internal_3x3_ch, internal_3x3_ch, (3, 3), internal_stride, (1, 1),
+                                         act=copy.deepcopy(act), reverse_order=True,
+                                         norm_layer=norm_name, adain_num_style_features=adain_num_style_features))
+
+        norm_name = 'bn' if bn else ('adain' if adain else None)  # 3rd layer: BN has the priority over AdaIN
+        self.res_convs.add_module('incr1x1',
+                                  Conv2D(internal_3x3_ch, out_ch, (1, 1), (1, 1), (0, 0),
+                                         act=copy.deepcopy(act), reverse_order=True,
+                                         norm_layer=norm_name, adain_num_style_features=adain_num_style_features))
+
+    def forward(self, x_and_w):
+        """
+        :param x_and_w: Tuple: Feature maps x, conditiong (style) vector w
+        :return: Output feature maps, conditiong (style) vector w
+        """
+        x, w = x_and_w[0], x_and_w[1]
+        res, _ = self.res_convs(x_and_w)
+
+        # if upsampling AND decreasing ch count: we'll use the method inspired by "the devil is in the decoder"
+        #  (https://arxiv.org/abs/1707.05847) -> bilinear (or bicubic) upsampling and add some channels (non-learnable)
+        # Upsampling and decreasing ch count should happen simultaneously, but this is not a constraint
+
+        # upsampling or downsampling
+
+        # Addition of residuals (number of in/out channels can be different)
+        if self.in_ch == self.out_ch:
+            x = x + res
+        elif self.in_ch < self.out_ch:  # Increasing number of channels (e.g. in an encoder)
+            # The new channels (non-existent in original input x) will include residual values only
+            res[:, 0:self.in_ch, :, :] = res[:, 0:self.in_ch, :, :] + x
+            x = res
+        elif self.in_ch > self.out_ch:  # Decreasing number of channels (e.g. in a decoder)
+            # Split input channels into out_ch groups, add (and normalize) all channels from each group
+            in_ch_split_indices = np.array_split(np.arange(0, self.in_ch), self.out_ch)
+            for out_ch_index, in_ch_indices in enumerate(in_ch_split_indices):
+                for in_ch_index in list(in_ch_indices):
+                    res[:, out_ch_index, :, :] += (x[:, in_ch_index, :, :] / in_ch_indices.shape[0])
+            x = res
+
+        return x, w
